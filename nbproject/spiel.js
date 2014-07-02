ffa54650312b8fcf78b78c89827d669ef508aee2
Informaster <Informaster@users.noreply.github.com>
2014-07-02 17:15:48 +0200

    Update spiel.js

--- nbproject/spiel.js
+++ nbproject/spiel.js
@@ -3,11 +3,11 @@
  * and open the template in the editor.
  */
 $(document).ready(function(){
-      $("body").("<div class='ueberschrift'>Hallo</div>, dies ist mein Spiel");
+      $("body").html("<div class='ueberschrift'>Hallo</div>, dies ist mein Spiel");
       $("body").append("<br> Dies ist eine weitere Zeile zur Demonstration der funktionierenden Funktionsweise dieser Unternehmung");
-      $("body").append("<br> BMI-Rechner  <p> Grösse : <input id='groesse' type='text'/> ");
+      $("body").append("<br> BMI-Rechner  <p> Grösse (m) : <input id='groesse' type='text'/> ");
       $("body").append("<br> Gewicht : <input id='gewicht' type='text' />");
-      $("body").append("<p> <input id='start' type='button' value='Berechnen'");
+      $("body").append("<p> <input id='start' type='button' value='Berechnen'/>");
       $("body").append("<div id='ausgabe' />");
       
       $("#start").click(function(){
